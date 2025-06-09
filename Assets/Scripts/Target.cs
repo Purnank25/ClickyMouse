8303d995cb23315e5cbf534812e478369202d61d
Purnank25 <purnankexcel1@gmail.com>
2025-06-09 17:05:16 +0530

    added target explosion

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -10,7 +10,7 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpwanPos = -1;
     public int pointValue;
-
+    public ParticleSystem explosionParticle;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -31,6 +31,7 @@ public class Target : MonoBehaviour
     {
         Destroy(gameObject);
         gameManager.UpdateScore(pointValue);
+        Instantiate(explosionParticle,transform.position, explosionParticle.transform.rotation);
     }
     private void OnTriggerEnter(Collider other)
     {
