4caf951752ed612cfc1fa773e15c41c83c3f36c9
Purnank25 <purnankexcel1@gmail.com>
2025-06-10 23:21:34 +0530

    added game over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,12 +10,16 @@ public class GameManager : MonoBehaviour
     public List<GameObject> targets;
     private float spwanRate = 1.0f;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
     private int score = 0;
+    public bool isGameActive;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpwanTarget());
         UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -25,7 +29,7 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator SpwanTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spwanRate);
             int index = UnityEngine.Random.Range(0, targets.Count);
@@ -38,6 +42,11 @@ public class GameManager : MonoBehaviour
         
         score += scoreToAdd;
         scoreText.text = "Score: " + score; 
+    }
+    public void GameOver ()
+    {
+        gameOverText.gameObject.SetActive(true);      
+        isGameActive = false;
     }
     
 }
